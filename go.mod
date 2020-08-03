7d8f1e67a4e7188f630460829df39a98d76ff0c1
OrangeLBlue <1398825239@qq.com>
2020-08-04 03:24:11 +0800

    MyAdd

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OrangLBlue/justtest
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+//MyAdd : add numbers
+func MyAdd(x, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println("helloworld")
+	fmt.Println(MyAdd(3, 4))
+}
